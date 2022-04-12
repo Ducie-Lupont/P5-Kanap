b8094302c0c081369d127aa1c51dbc1ee632c02d
Ducie-Lupont <dupont.lucie.018@gmail.com>
2022-04-12 19:14:28 +0200

    Fin page produit début page panier

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,3 +1,32 @@
-products();
-localStorage["products"];
-console.log(products);
+let arrayProductsInCart = JSON.parse(localStorage.getItem("products"))
+console.log(arrayProductsInCart)
+const cartItems = document.querySelector("#cart__items")
+
+
+if (arrayProductsInCart) {
+    for (let cartContent of arrayProductsInCart) {
+        fetch(`http://localhost:3000/api/products/${cartContent.cartId}`).then((response) =>
+            response.json().then((data) => {
+                displayCart(cartContent, data)
+            }))
+    }
+}
+
+
+function displayCart(cartContent, data) {
+    let cartItem = document.createElement("article");
+    let cartItemDivImg = document.createElement("div");
+    let cartItemImg = document.createElement("img");
+    let cartItemDivContent = document.createElement("div");
+    let cartItemDivContentDesc = document.createElement("div");
+    let cartItemProductName = document.createElement("h2");
+    let cartItemProductColor = document.createElement("p");
+    let cartItemProductPrice = document.createElement("p");
+    let DivContentSettings = document.createElement("div");
+    let DivContentSettingsQuantity = document.createElement("div");
+    let ContentSettingsQuantityValue = document.createElement("p");
+    let ContentSettingsQuantityInput = document.createElement("input");
+    let ContentSettingsDelete = document.createElement("div");
+    let ContentSettingsDeleteItem = document.createElement("p");
+    
+}
